99c388e4b77d9843ccbccb1128de940ef188c214
AminovaAnna <aminovaanna163@gmail.com>
2024-04-30 17:22:00 +0300

    дз потоки данных

--- src/main/java/ru/hogwarts/school/Anna/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/Anna/repository/FacultyRepository.java
@@ -1,10 +1,11 @@
 package ru.hogwarts.school.Anna.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.Anna.model.Faculty;
 
 import java.util.Collection;
-
+@Repository
 public interface FacultyRepository extends JpaRepository<Faculty,Long> {
     Collection<Faculty> findAllByColorIgnoreCaseOrNameIgnoreCase (String color, String name);
 
