5d7efb87a6236fdfbddc8a5726e509bda17758f5
AminovaAnna <aminovaanna163@gmail.com>
2024-05-21 19:44:13 +0300

    прошу проверить домашнюю работу по теме Стримы и потоки

--- src/main/java/ru/hogwarts/school/Anna/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/Anna/controller/StudentController.java
@@ -8,7 +8,7 @@ import ru.hogwarts.school.Anna.service.StudentService;
 import java.util.Collection;
 
 @RestController
-@RequestMapping(path = "/student" )
+@RequestMapping(path = "/student")
 public class StudentController {
     private final StudentService studentService;
 
@@ -17,55 +17,72 @@ public class StudentController {
     }
 
     @GetMapping
-    public Student getStudentById(@RequestParam long id){
+    public Student getStudentById(@RequestParam long id) {
         return studentService.getStudentById(id);
     }
+
     @PostMapping
-    public Student addStudent(@RequestBody Student student){
+    public Student addStudent(@RequestBody Student student) {
         return studentService.addStudent(student);
     }
+
     @PutMapping
-    public Student updateStudent(@RequestBody Student student){
+    public Student updateStudent(@RequestBody Student student) {
         return studentService.updateStudent(student);
     }
+
     @DeleteMapping
-    public boolean deleteStudent(@RequestParam long id){
+    public boolean deleteStudent(@RequestParam long id) {
         return studentService.deleteStudent(id);
     }
+
     @GetMapping(path = "/byAge")
-    public Collection<Student> getByAgeBetween (@RequestParam(required = false) Integer min,
-                                                @RequestParam(required = false) Integer max){
-        if (min != null && max != null){
-            return studentService.getByAgeBetween (min, max);
+    public Collection<Student> getByAgeBetween(@RequestParam(required = false) Integer min,
+                                               @RequestParam(required = false) Integer max) {
+        if (min != null && max != null) {
+            return studentService.getByAgeBetween(min, max);
         }
         return studentService.getAll();
     }
+
     @GetMapping(path = "/facultyByStudent")
-    public Faculty getStudentFacultyByStudent(@RequestParam long studentId){
+    public Faculty getStudentFacultyByStudent(@RequestParam long studentId) {
         return studentService.getStudentById(studentId).getFaculty();
     }
 
     @GetMapping("/count")
-    public int getStudentCount(){
+    public int getStudentCount() {
         return studentService.getStudentCount();
     }
 
     @GetMapping("/avg-age")
-    public double getAvgAge(){
+    public double getAvgAge() {
         return studentService.getAvgAge();
     }
 
     @GetMapping("/last")
-    public Collection<Student> getLastStudents(){
+    public Collection<Student> getLastStudents() {
         return studentService.getLastFive();
     }
+
     @GetMapping("/nameStartsA")
-    public Collection<String> getStudentsNameStartsA(){
+    public Collection<String> getStudentsNameStartsA() {
         return studentService.getNameStartsWithA();
     }
+
     @GetMapping("/avg-age-stream")
-    public double getAverageAgeStream(){
+    public double getAverageAgeStream() {
         return studentService.getAverageAge();
+    }
+
+    @GetMapping("/print-parallel")
+    public void printParallel() {
+        studentService.printParallel();
+    }
+
+    @GetMapping("/print-sync")
+    public void printSync(){
+        studentService.printSync();
     }
 
 }
--- src/main/java/ru/hogwarts/school/Anna/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/Anna/service/StudentService.java
@@ -8,6 +8,7 @@ import ru.hogwarts.school.Anna.model.Student;
 import ru.hogwarts.school.Anna.repository.StudentRepository;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,7 +27,7 @@ public class StudentService {
     }
 
     public Student getStudentById(long id) {
-        try{
+        try {
             logger.info("Student has been received id=" + id);
             return repository.findById(id).orElseThrow(RecordNotFoundException::new);
         } catch (RecordNotFoundException e) {
@@ -51,7 +52,7 @@ public class StudentService {
                 }).orElse(false);
     }
 
-    public Collection<Student> getByAgeBetween (Integer min, Integer max) {
+    public Collection<Student> getByAgeBetween(Integer min, Integer max) {
         logger.info("Students with ages from min={} to max={} received", min, max);
         return repository.findAllByAgeBetween(min, max);
     }
@@ -61,33 +62,79 @@ public class StudentService {
         return repository.findAll();
     }
 
-    public int getStudentCount(){
+    public int getStudentCount() {
         logger.info("Count of students received");
         return repository.countStudents();
     }
-    public double getAvgAge(){
+
+    public double getAvgAge() {
         logger.info("Average age of students requested");
         return repository.avgAge();
     }
 
-    public Collection<Student> getLastFive(){
+    public Collection<Student> getLastFive() {
         logger.info("Last five students received");
         return repository.getLastFive();
     }
-    public Collection<String> getNameStartsWithA(){
+
+    public Collection<String> getNameStartsWithA() {
         return repository.findAll().stream()      //findAllByFacultyId??
                 .map(Student::getName)
                 .map(String::toUpperCase)
-                .filter(name->name.startsWith("A"))
+                .filter(name -> name.startsWith("A"))
                 .sorted()
                 .toList();
     }
 
-    public double getAverageAge(){
+    public double getAverageAge() {
         return repository.findAll().stream()
                 .mapToDouble(Student::getAge)
                 .average()
                 .orElse(0);
+    }
+
+    public void printParallel() {
+        var students = repository.findAll();
+        logger.info(students.get(0).toString());
+        logger.info(students.get(1).toString());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(8000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                logger.info(students.get(2).toString());
+                logger.info(students.get(3).toString());
+            }
+        }).start();
+
+        new Thread(() -> {
+            logger.info(students.get(4).toString());
+            logger.info(students.get(5).toString());
+        }).start();
+    }
+
+    public void printSync() {
+        var students = repository.findAll();
+        print(students.get(0));
+        print(students.get(1));
+        new Thread(() -> {
+            print(students.get(2));
+            print(students.get(3));
+        }).start();
+
+        new Thread(() -> {
+            print(students.get(4));
+            print(students.get(5));
+        }).start();
+    }
+
+
+    private synchronized void print(Object o) {
+        logger.info(o.toString());
     }
 
 }
